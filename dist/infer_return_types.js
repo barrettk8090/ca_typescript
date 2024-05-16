5cd7956d939b6596885b444fb445f7453881c067
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-05-16 08:21:29 -0600

    infer return types

--- dist/infer_return_types.js
+++ dist/infer_return_types.js
@@ -1,7 +1,12 @@
 "use strict";
 function createGreeting(name) {
-    console.log("Hello, ".concat(name));
+    console.log("Hello, ".concat(name, "!"));
     return "Hello, ".concat(name, "!");
 }
 var myGreeting = createGreeting('Barrett K');
+function getRandomNumber() {
+    return Math.random();
+}
+var myVar = getRandomNumber();
+// Infers that myVar is type: number due to the return statement in getRandomNumber
 //# sourceMappingURL=infer_return_types.js.map
--- infer_return_types.ts
+++ infer_return_types.ts
@@ -4,3 +4,10 @@ function createGreeting(name:string) {
 }
 
 const myGreeting = createGreeting('Barrett K')
+
+function getRandomNumber() {
+    return Math.random();
+}
+
+let myVar = getRandomNumber()
+// Infers that myVar is type: number due to the return statement in getRandomNumber
