f38c74beafcd23aa8f95a2da855a9658683a93fe
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-05-20 07:21:51 -0600

    spread syntax

--- array_type_annotation.ts
+++ array_type_annotation.ts
@@ -29,3 +29,28 @@ function addPower(p: number, ...numsToAdd: number[]):number {
   }
 
 addPower(2, 3, 4)
+
+//Spread Syntax
+function performDanceMove(moveName:string, moveReps:number, hasFlair:boolean):void{
+    console.log(`I do the ${moveName} ${moveReps} times !`);
+    if(hasFlair){
+      console.log('I do it with flair!');
+    }
+  }
+  
+  let danceMoves: [string, number, boolean][] = [
+    ['chicken beak', 4, false],
+    ['wing flap', 4, false],
+    ['tail feather shake', 4, false],
+    ['clap', 4, false],
+    ['chicken beak', 4, true],
+    ['wing flap', 4, true],
+    ['tail feather shake', 4, true],
+    ['clap', 4, true],
+  ];
+  
+//   Using spread syntax ... to expand the iterable elements of dance moves arrays into individual elements from the performDanceMoves function
+  danceMoves.forEach(move => performDanceMove(...move))
+
+  //Like calling:
+  //performDanceMovie('chicken beak', 4, false)
